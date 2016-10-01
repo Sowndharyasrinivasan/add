22f22488005355a614f3d4ba4e982b942f2c52c8
Sowndharyasrinivasan <sowndharyasri06@gmail.com>
2016-10-01 10:18:28 +0530

    Add files via upload

---
+++ Position.java
@@ -0,0 +1,20 @@
+public static void main(String args[]) {
+        int size = 2;     
+        generateTable(0, size, new int[size]);
+    }
+
+    private static void generateTable(int index, int size, int[] current) {
+        if (index == size) {
+            for (int a = 0; a < size; a++) {
+                System.out.print(current[a] + " ");
+            }
+            System.out.println();
+        } else {
+            for (int a = 0; a < 2; a++) {
+                current[index] = a;
+                generateTable(index + 1, size, current);
+            }
+        }
+}
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Status Help
